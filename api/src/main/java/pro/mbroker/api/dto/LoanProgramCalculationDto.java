9c79488928a662f214c239fdc4b7f8f7f07b442c
Alexander_Lindemann <a_lindemann@icloud.com>
2023-04-19 13:40:33 +0300

    MB-51 recreate api calculator on get request

--- api/src/main/java/pro/mbroker/api/dto/LoanProgramCalculationDto.java
+++ api/src/main/java/pro/mbroker/api/dto/LoanProgramCalculationDto.java
@@ -7,11 +7,15 @@ import lombok.ToString;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Getter
 @Setter
 @ToString
 public class LoanProgramCalculationDto {
+    private UUID creditProgramId;
+
+    private String creditProgramName;
 
     @DecimalMin(value = "0.00", inclusive = true, message = "Base rate cannot be negative")
     @DecimalMax(value = "100.00", inclusive = true, message = "Base rate cannot be greater than 100.00")
