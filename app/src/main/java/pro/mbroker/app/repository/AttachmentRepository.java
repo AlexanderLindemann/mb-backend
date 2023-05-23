791010e9836e0b274e3267d0ad537dc4fad5ecc2
verbitskii <a.verbitskii@cn-innov.ru>
2023-05-23 17:14:02 +0300

    MB-52 Загружать, получать логотипы банков

--- app/src/main/java/pro/mbroker/app/repository/AttachmentRepository.java
+++ app/src/main/java/pro/mbroker/app/repository/AttachmentRepository.java
@@ -3,5 +3,10 @@ package pro.mbroker.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.mbroker.app.entity.Attachment;
 
+import java.util.Optional;
+
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+    Optional<Attachment> findAttachmentByExternalStorageId(Long externalId);
+
 }
