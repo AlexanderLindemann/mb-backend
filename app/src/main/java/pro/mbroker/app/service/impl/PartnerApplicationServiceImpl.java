700622f553f279f50ef9569b741f6c91f4154aa7
Alexander_Lindemann <a_lindemann@icloud.com>
2023-06-30 17:32:42 +0800

    bugfix MB-245 fix statuses

--- app/src/main/java/pro/mbroker/app/service/impl/PartnerApplicationServiceImpl.java
+++ app/src/main/java/pro/mbroker/app/service/impl/PartnerApplicationServiceImpl.java
@@ -449,11 +449,7 @@ public class PartnerApplicationServiceImpl implements PartnerApplicationService 
         if (allProfilesDataEntered) {
             List<BankApplication> bankApplications = partnerApplication.getBankApplications();
             for (BankApplication bankApplication : bankApplications) {
-                if (!bankApplication.getBankApplicationStatus().equals(BankApplicationStatus.SENT_TO_BANK) ||
-                        !bankApplication.getBankApplicationStatus().equals(BankApplicationStatus.SENDING_TO_BANK) ||
-                        !bankApplication.getBankApplicationStatus().equals(BankApplicationStatus.APPLICATION_APPROVED) ||
-                        !bankApplication.getBankApplicationStatus().equals(BankApplicationStatus.EXPIRED) ||
-                        !bankApplication.getBankApplicationStatus().equals(BankApplicationStatus.READY_TO_SENDING)) {
+                if (!bankApplication.getBankApplicationStatus().equals(BankApplicationStatus.SENT_TO_BANK)) {
                     bankApplication.setBankApplicationStatus(BankApplicationStatus.READY_TO_SENDING);
                 }
             }
