48bdaebff2073cb7ecbcdb32c7e22e7425efd1cf
Alexander_Lindemann <a_lindemann@icloud.com>
2023-08-10 17:32:10 +0800

    bugfix_MB-374 сортировка по дате изменения PartnerApplication

--- app/src/main/java/pro/mbroker/app/util/BaseEntityAuditAspect.java
+++ app/src/main/java/pro/mbroker/app/util/BaseEntityAuditAspect.java
@@ -6,54 +6,61 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import pro.mbroker.app.entity.*;
+import pro.mbroker.app.repository.PartnerApplicationRepository;
 import pro.smartdeal.ng.common.security.service.CurrentUserService;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class BaseEntityAuditAspect {
+
     @NonNull
     private final CurrentUserService currentUserService;
+    private final PartnerApplicationRepository partnerApplicationRepository;
 
     @Before("execution(* pro.mbroker.app.repository.*.save(..)) && args(baseEntity, ..)")
     public void beforeSave(BaseEntity baseEntity) {
         setAuditFields(baseEntity);
+        if (baseEntity instanceof MortgageCalculation) {
+            PartnerApplication partnerApplication = partnerApplicationRepository.findByMortgageCalculation((MortgageCalculation) baseEntity);
+            if (partnerApplication != null && partnerApplication.isActive()) {
+                setAuditFields(partnerApplication);
+            }
+            return;
+        }
+
+        if (baseEntity instanceof BankApplication) {
+            setAuditFields(((BankApplication) baseEntity).getPartnerApplication());
+            return;
+        }
+
+        if (baseEntity instanceof BorrowerProfile) {
+            setAuditFields(((BorrowerProfile) baseEntity).getPartnerApplication());
+            return;
+        }
+
         if (baseEntity instanceof PartnerApplication) {
             PartnerApplication partnerApplication = (PartnerApplication) baseEntity;
-            if (partnerApplication.getBankApplications() != null) {
-                for (BankApplication bankApplication : partnerApplication.getBankApplications()) {
-                    setAuditFields(bankApplication);
-                }
-            }
-            if (partnerApplication.getBorrowerProfiles() != null) {
-                for (BorrowerProfile borrowerProfile : partnerApplication.getBorrowerProfiles()) {
-                    setAuditFields(borrowerProfile);
-                }
-            }
+            partnerApplication.getBankApplications().forEach(this::setAuditFields);
+            partnerApplication.getBorrowerProfiles().forEach(this::setAuditFields);
             if (partnerApplication.getMortgageCalculation() != null) {
                 setAuditFields(partnerApplication.getMortgageCalculation());
             }
+            return;
         }
-        if (baseEntity instanceof Bank) {
-            Bank bank = (Bank) baseEntity;
-            if (bank.getContacts() != null) {
-                for (BankContact bankContact : bank.getContacts()) {
-                    setAuditFields(bankContact);
-                }
-            }
+
+        if (baseEntity instanceof Bank && ((Bank) baseEntity).getContacts() != null) {
+            ((Bank) baseEntity).getContacts().forEach(this::setAuditFields);
+            return;
         }
-        if (baseEntity instanceof Partner) {
-            Partner partner = (Partner) baseEntity;
-            if (partner.getRealEstates() != null) {
-                for (RealEstate realEstate : partner.getRealEstates()) {
-                    setAuditFields(realEstate);
-                }
-            }
+
+        if (baseEntity instanceof Partner && ((Partner) baseEntity).getRealEstates() != null) {
+            ((Partner) baseEntity).getRealEstates().forEach(this::setAuditFields);
         }
     }
-
 
     @Before("execution(* pro.mbroker.app.repository.*.saveAll(..)) && args(entities, ..)")
     public void beforeSaveAll(Collection<BaseEntity> entities) {
@@ -65,8 +72,10 @@ public class BaseEntityAuditAspect {
         int sdId = extractSdIdFromToken(currentUserToken);
         if (baseEntity.getCreatedBy() == null) {
             baseEntity.setCreatedBy(sdId);
+            baseEntity.setCreatedAt(LocalDateTime.now());
         } else {
             baseEntity.setUpdatedBy(sdId);
+            baseEntity.setUpdatedAt(LocalDateTime.now());
         }
     }
 
@@ -74,4 +83,5 @@ public class BaseEntityAuditAspect {
         return TokenExtractor.extractSdId(token);
     }
 }
+
 
