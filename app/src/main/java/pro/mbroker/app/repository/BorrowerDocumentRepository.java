cded96593ef2edc01bd0ab498be2a768dac0b464
Седаш Юлия Анатольевна <y.sedash@smartdeal.pro>
2023-10-28 15:53:27 +0300

    Pull request #297: MB-606 anable ros api
    Merge in MB/mb-backend from MB-606_anable_ros_api to develop
    
    Squashed commit of the following:
    
    commit d2c9409028d8f4b95f919cdea2063f8a58656faa
    Author: u.sedash <shteker06@gmail.com>
    Date:   Sat Oct 28 16:40:59 2023 +0400
    
        [MB-606] enable_ros_api
    
    commit 7efb865ef57a740c3ca07adad08533d6346a8be2
    Author: u.sedash <shteker06@gmail.com>
    Date:   Wed Oct 25 08:42:30 2023 +0400
    
        [MB-606] enable_ros_api

--- app/src/main/java/pro/mbroker/app/repository/BorrowerDocumentRepository.java
+++ app/src/main/java/pro/mbroker/app/repository/BorrowerDocumentRepository.java
@@ -16,8 +16,20 @@ public interface BorrowerDocumentRepository extends JpaRepository<BorrowerDocume
     @Query(value = "SELECT bd.attachment.id " +
             "FROM BorrowerDocument bd " +
             "WHERE bd.isActive = true AND (" +
-            "(bd.bankApplication.id = :bankApplicationId AND bd.borrowerProfile.id IN :borrowerProfileIds) OR " +
-            "(bd.borrowerProfile.id IN :borrowerProfileIds)) ")
+            "        (bd.bankApplication.id = :bankApplicationId AND bd.borrowerProfile.id IN :borrowerProfileIds) " +
+            "        OR" +
+            "        (bd.borrowerProfile.id IN :borrowerProfileIds) " +
+            "    ) " +
+            "  AND (bd.borrowerProfile.id, bd.updatedAt, bd.documentType) IN (" +
+            "    SELECT bd2.borrowerProfile.id, MAX(bd2.updatedAt), bd2.documentType " +
+            "    FROM BorrowerDocument bd2 " +
+            "    WHERE bd2.isActive = true " +
+            "      AND (bd2.bankApplication.id = :bankApplicationId " +
+            "                AND bd2.borrowerProfile.id IN :borrowerProfileIds"  +
+            "            OR" +
+            "            (bd2.borrowerProfile.id IN :borrowerProfileIds)" +
+            "        )\n" +
+            "    GROUP BY bd2.borrowerProfile.id, bd2.documentType)     ")
     Set<Long> getAttachments(@Param("bankApplicationId") UUID bankApplicationId,
                              @Param("borrowerProfileIds") List<UUID> borrowerProfileIds);
 }
