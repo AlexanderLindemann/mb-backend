5bc4cf894615f327140817664c216fd7b437b241
Александр Олегович Линдеманн <a.lindemann@smartdeal.pro>
2024-01-30 14:49:58 +0300

    Pull request #421: MB-850 правлю нотификейшен
    Merge in MB/mb-backend from MB-850 to develop
    
    Squashed commit of the following:
    
    commit a9c71978f740b090475d4044c6ac9caa8c3537be
    Author: Alexander_Lindemann <a_lindemann@icloud.com>
    Date:   Tue Jan 30 18:30:51 2024 +0700
    
        MB-850 правлю нотификейшен

--- api/src/main/java/pro/mbroker/api/dto/response/AttachmentInfo.java
+++ api/src/main/java/pro/mbroker/api/dto/response/AttachmentInfo.java
@@ -2,8 +2,10 @@ package pro.mbroker.api.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AttachmentInfo {
 
